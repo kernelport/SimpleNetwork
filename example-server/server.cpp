c633d6e60b6ec604073fd5cac3d314cdb1fd5b97
Frank Neuber <fn@kernelport.com>
2019-04-26 18:19:43 +0200

    implement unlimited multi thread connections

--- example-server/server.cpp
+++ example-server/server.cpp
@@ -1,35 +1,35 @@
 #include <iostream>
+#include <sys/socket.h>
 #include "TCPServer.h"
 
-TCPServer tcp;
+using namespace std;
 
-void * loop(void * m)
-{
-        pthread_detach(pthread_self());
-	while(1)
-	{
-		srand(time(NULL));
-		char ch = 'a' + rand() % 26;
-		string s(1,ch);
-		string str = tcp.getMessage();
-		if( str != "" )
-		{
-			cout << "Message:" << str << endl;
-			tcp.Send(" [client message: "+str+"] "+s);
-			tcp.clean();
-		}
-		usleep(1000);
-	}
-	tcp.detach();
+class RpcTCPServer:  public TCPServer {
+public:
+    RpcTCPServer(int port) : TCPServer(port) {}
+    int handleMessage(int newsockfd);
+};
+
+int RpcTCPServer::handleMessage(int newsockfd) {
+    int n;
+    char msg[MAXPACKETSIZE];
+    n = recv(newsockfd, msg, MAXPACKETSIZE, 0);
+    cout << " recv: " << msg << endl;
+    if (n <= 0) {
+        cout << " handleMessage: " << n << endl;
+        return n;
+    }
+    msg[n] = 0;
+    //Message = string(msg);
+    string sendstr = "sending RpcTCPServer " + string(msg) ;
+    send(newsockfd, sendstr.c_str(), sendstr.length(), 0);
+    return n;
 }
 
 int main()
 {
-	pthread_t msg;
-	tcp.setup(11999);
-	if( pthread_create(&msg, NULL, loop, (void *)0) == 0)
-	{
-		tcp.receive();
-	}
+	RpcTCPServer tcp(11999);
+        tcp.run();
+        while (1);
 	return 0;
 }
--- src/TCPServer.cpp
+++ src/TCPServer.cpp
@@ -1,72 +1,90 @@
+#include <thread>
+#include <netinet/tcp.h> // ‘TCP_NODELAY’
+
+#include <iostream>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+
 #include "TCPServer.h" 
 
-string TCPServer::Message;
+TCPServer::TCPServer(int port) {
+    struct sockaddr_in serverAddress;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&serverAddress, 0, sizeof (serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddress.sin_port = htons(port);
+    int optValue = 1; // DO NOT make this a "bool" type! I have seen it not working then.
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optValue, sizeof (optValue));
+    bind(sockfd, (struct sockaddr *) &serverAddress, sizeof (serverAddress));
+    listen(sockfd, 5);
+}
 
-void* TCPServer::Task(void *arg)
-{
-	int n;
-	int newsockfd = (long)arg;
-	char msg[MAXPACKETSIZE];
-	pthread_detach(pthread_self());
-	while(1)
-	{
-		n=recv(newsockfd,msg,MAXPACKETSIZE,0);
-		if(n==0)
-		{
-		   close(newsockfd);
-		   break;
-		}
-		msg[n]=0;
-		//send(newsockfd,msg,n,0);
-		Message = string(msg);
+TCPServer::~TCPServer() {
+    std::cout << "TCPServer::~TCPServer called: " << std::this_thread::get_id() << std::endl;
+    close(sockfd);
+    sockfd = -1;
+}
+
+void TCPServer::Task(void * This, int newsockfd) {
+    std::cout << "TCPServer::Task start: " << std::this_thread::get_id() << std::endl;
+    while (1) {
+        int n = ((TCPServer *) This)->handleMessage(newsockfd);
+        if (n <= 0) {
+            close(newsockfd);
+            break;
         }
-	return 0;
+    }
+    std::cout << " TCPServer::Task stop: " << std::this_thread::get_id() << std::endl;
 }
 
-void TCPServer::setup(int port)
-{
-	sockfd=socket(AF_INET,SOCK_STREAM,0);
- 	memset(&serverAddress,0,sizeof(serverAddress));
-	serverAddress.sin_family=AF_INET;
-	serverAddress.sin_addr.s_addr=htonl(INADDR_ANY);
-	serverAddress.sin_port=htons(port);
-        int optValue = 1; // DO NOT make this a "bool" type! I have seen it not working then.
-        setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optValue, sizeof(optValue));
-	bind(sockfd,(struct sockaddr *)&serverAddress, sizeof(serverAddress));
- 	listen(sockfd,5);
+void TCPServer::run() {
+
+    class MessageServer {
+    public:
+
+        static void accept_conn(void * This, int sockfd) {
+            std::thread accept_thread([This, sockfd] {
+                while (1) {
+                    struct sockaddr_in clientAddress;
+                    std::string str;
+                    socklen_t sosize = sizeof (clientAddress);
+                    int newsockfd = accept(sockfd, (struct sockaddr*) &clientAddress, &sosize);
+                    if (newsockfd <= 0) {
+                        break;
+                    }
+                    std::thread t(Task, This, newsockfd);
+                    t.detach();
+                    str = inet_ntoa(clientAddress.sin_addr);
+                    std::cout << "TCPServer::run MessageServer::accept_conn: " << str << std::endl;
+                }
+            });
+            accept_thread.detach();
+        }
+    };
+    MessageServer::accept_conn(this, sockfd);
 }
 
-string TCPServer::receive()
-{
-	string str;
-	while(1)
-	{
-		socklen_t sosize  = sizeof(clientAddress);
-		newsockfd = accept(sockfd,(struct sockaddr*)&clientAddress,&sosize);
-		str = inet_ntoa(clientAddress.sin_addr);
-		pthread_create(&serverThread,NULL,&Task,(void *)newsockfd);
-	}
-	return str;
-}
+int TCPServer::handleMessage(int newsockfd) {
+    int n;
+    char msg[MAXPACKETSIZE];
 
-string TCPServer::getMessage()
-{
-	return Message;
-}
+    struct timeval tv;
+    tv.tv_sec = 8; /* Secs Timeout */
+    tv.tv_usec = 0; // Not init'ing this can cause strange errors
+    setsockopt(newsockfd, SOL_SOCKET, SO_RCVTIMEO, (char *) &tv, sizeof (struct timeval));
+    int flag = 1;
+    if (setsockopt(newsockfd, IPPROTO_TCP, TCP_NODELAY, (char *) &flag, sizeof (int)) < 0) {
+        std::cout << "TCPServer::handleMessage Could not disable Nagle's algorithm." << std::endl;
+    }
 
-void TCPServer::Send(string msg)
-{
-	send(newsockfd,msg.c_str(),msg.length(),0);
+    n = recv(newsockfd, msg, MAXPACKETSIZE, 0);
+    if (n <= 0) {
+        return n;
+    }
+    msg[n] = 0;
+    std::string sendstr = "sending TCPServer";
+    send(newsockfd, sendstr.c_str(), sendstr.length(), 0);
+    return n;
 }
-
-void TCPServer::clean()
-{
-	Message = "";
-	memset(msg, 0, MAXPACKETSIZE);
-}
-
-void TCPServer::detach()
-{
-	close(sockfd);
-	close(newsockfd);
-} 
